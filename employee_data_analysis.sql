538c5bae861a6e05e90bb5ffa57aec2307a945fb
Papia Karjee <54677114+papia-karjee@users.noreply.github.com>
2026-03-04 14:29:11 +0530

    Update employee_data_analysis.sql

--- employee_data_analysis.sql
+++ employee_data_analysis.sql
@@ -1,123 +1,171 @@
-use papia
-create table employees_records(
-	emp_id int primary key,
-	emp_name varchar(30),
-	dept varchar(30),
-	salary decimal(10,2),
-	manager_id int foreign key references employees_records(emp_id),
-	hire_date date
-)
 
-insert into employees_records values(101,'Sindura','IT',50000,Null,'2025-05-05'),
-                                 (102,'Aakash','CSE',70000,101,'2025-03-15'),
-								   (103,'Raja','Finance',60000,101,'2024-08-15'),
-								     (104,'Raktim','EE',40000,102,'2023-07-25'),
-									   (105,'Sampurna','Finance',50000,103,'2022-04-25'),
-									     (106,'Divisha','IT',40000,104,'2021-08-15'),
-										  (107,'Sindura','IT',50000,106,'2025-05-05')
+-- Create Employee Table
+CREATE TABLE employees_records (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(30),
+    dept VARCHAR(30),
+    salary DECIMAL(10,2),
+    manager_id INT FOREIGN KEY REFERENCES employees_records(emp_id),
+    hire_date DATE
+);
 
+-- Insert Data
+INSERT INTO employees_records VALUES
+(101,'Sindura','IT',50000,NULL,'2025-05-05'),
+(102,'Aakash','CSE',70000,101,'2025-03-15'),
+(103,'Raja','Finance',60000,101,'2024-08-15'),
+(104,'Raktim','EE',40000,102,'2023-07-25'),
+(105,'Sampurna','Finance',50000,103,'2022-04-25'),
+(106,'Divisha','IT',40000,104,'2021-08-15'),
+(107,'Sindura','IT',50000,106,'2025-05-05');
 
-
-select * from employees_records
-
----- find the second highest salary
-select max(salary) as second_highest_salary from employees_records
-                    where salary <(select max(salary) from employees_records)
+-- View Table
+SELECT * FROM employees_records;
 
 
----- find the third highest salary
-select max(salary)as third_highest_salary 
-                from employees_records 
-				     where salary <(select max(salary) from employees_records 
-					    where salary <(select max(salary) from employees_records))
-
----- find employees who earn more than their manager
-select e.emp_name as employee_name,
-        m.emp_name as manager_name,
-		  e.salary as employee_salary,
-		    m.salary as manager_salary from employees_records e join employees_records m on e.manager_id = m.emp_id   
-			         where e.salary > m.salary
-
----- count number of employees in each dept
-select dept,
-       count(emp_id)as total_employees
-	      from employees_records group by dept order by total_employees desc
-
----- find departments having more than 5 employees
-
-select dept,
-        count(emp_id)as Employees_Count 
-		    from employees_records group by dept having count(emp_id)>5
-
-select * from   employees_records
----- find duplicate employee names
-select *from(
-select emp_name,
-        emp_id,
-		row_number()over(partition by emp_name order by emp_id)as rnk
-		   from employees_records)t
-		     where rnk > 1
-
-        
+------------------------------------------------
+-- Find Second Highest Salary
+------------------------------------------------
+SELECT MAX(salary) AS second_highest_salary
+FROM employees_records
+WHERE salary < (
+    SELECT MAX(salary) FROM employees_records
+);
 
 
----- delete duplicate records but keep one copy
-with cte as(
-select * from(
-   select emp_id,
-           emp_name,
-		      dept,
-			   salary,row_number()over(partition by emp_name order by emp_id)as duplicate_records
-			      from employees_records)t
-		)
-delete from cte where duplicate_records>1
+------------------------------------------------
+-- Find Third Highest Salary
+------------------------------------------------
+SELECT MAX(salary) AS third_highest_salary
+FROM employees_records
+WHERE salary < (
+    SELECT MAX(salary)
+    FROM employees_records
+    WHERE salary < (
+        SELECT MAX(salary)
+        FROM employees_records
+    )
+);
 
----- find employees hired in the last 6 months
-select emp_name,
-           salary,
-		       hire_date from employees_records  where hire_date <= dateadd(Month,-6,getdate())
 
---- find dept with highest average salary
-select dept,
+------------------------------------------------
+-- Find Employees Who Earn More Than Their Manager
+------------------------------------------------
+SELECT 
+    e.emp_name AS employee_name,
+    m.emp_name AS manager_name,
+    e.salary AS employee_salary,
+    m.salary AS manager_salary
+FROM employees_records e
+JOIN employees_records m
+ON e.manager_id = m.emp_id
+WHERE e.salary > m.salary;
+
+
+------------------------------------------------
+-- Count Number of Employees in Each Department
+------------------------------------------------
+SELECT 
+    dept,
+    COUNT(emp_id) AS total_employees
+FROM employees_records
+GROUP BY dept
+ORDER BY total_employees DESC;
+
+
+------------------------------------------------
+-- Find Departments Having More Than 5 Employees
+------------------------------------------------
+SELECT 
+    dept,
+    COUNT(emp_id) AS Employees_Count
+FROM employees_records
+GROUP BY dept
+HAVING COUNT(emp_id) > 5;
+
+
+------------------------------------------------
+-- Find Duplicate Employee Names
+------------------------------------------------
+SELECT *
+FROM (
+    SELECT 
         emp_name,
-		  avg(salary)over(partition by dept)as average_salary
-		      from employees_records
+        emp_id,
+        ROW_NUMBER() OVER(PARTITION BY emp_name ORDER BY emp_id) AS rnk
+    FROM employees_records
+) t
+WHERE rnk > 1;
 
 
---- find top 3 highest paid employees
-select top(3)emp_name,
-        salary as highest_paid_salary 
-		   from employees_records 
-		      
-		             order by highest_paid_salary  desc
-
---- using windows functions
-select * from(
-select emp_name,
-           salary,
-		    dense_rank()over(order by salary desc)as rnk
-			     from employees_records)t
-				     where rnk <=3
-
----- find employees whose name starts and ends with vowel
-select emp_name from employees_records where emp_name like 
-                        '[aeiou]%' and 
-						    emp_name like '%[aeiou]';
+------------------------------------------------
+-- Delete Duplicate Records But Keep One Copy
+------------------------------------------------
+WITH cte AS (
+    SELECT *
+    FROM (
+        SELECT 
+            emp_id,
+            emp_name,
+            dept,
+            salary,
+            ROW_NUMBER() OVER(PARTITION BY emp_name ORDER BY emp_id) AS duplicate_records
+        FROM employees_records
+    ) t
+)
+DELETE FROM cte
+WHERE duplicate_records > 1;
 
 
+------------------------------------------------
+-- Find Employees Hired in the Last 6 Months
+------------------------------------------------
+SELECT 
+    emp_name,
+    salary,
+    hire_date
+FROM employees_records
+WHERE hire_date >= DATEADD(MONTH,-6,GETDATE());
 
 
+------------------------------------------------
+-- Find Department with Highest Average Salary
+------------------------------------------------
+SELECT 
+    dept,
+    emp_name,
+    AVG(salary) OVER(PARTITION BY dept) AS average_salary
+FROM employees_records;
 
 
+------------------------------------------------
+-- Find Top 3 Highest Paid Employees
+------------------------------------------------
+SELECT TOP (3)
+    emp_name,
+    salary AS highest_paid_salary
+FROM employees_records
+ORDER BY salary DESC;
 
 
+------------------------------------------------
+-- Using Window Function (Dense Rank)
+------------------------------------------------
+SELECT *
+FROM (
+    SELECT 
+        emp_name,
+        salary,
+        DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+    FROM employees_records
+) t
+WHERE rnk <= 3;
 
 
-
-
- 
-
-
-
-                             
-
+------------------------------------------------
+-- Find Employees Whose Name Starts and Ends with a Vowel
+------------------------------------------------
+SELECT emp_name
+FROM employees_records
+WHERE emp_name LIKE '[aeiou]%'
+AND emp_name LIKE '%[aeiou]';
